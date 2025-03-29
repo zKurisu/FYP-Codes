a86f8742e5fa5a3e72e855e2c8d7c450c876880a
Jie Orkarin <2694783083@qq.com>
2025-03-29 12:43:24 +0800

    add: basic content of JS, CSS, example data input

--- rest/test/main.py
+++ rest/test/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import json
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,6 +14,186 @@ def main_page(request: Request):
         "index.html",
         {
             "request": request,
-            "ryu_wsgi": "http://127.0.0.1:8080"
+            "ryu_wsgi": "http://127.0.0.1:8000"
         }
     )
+    
+######### TMP TEST
+# Node List: ["node1", "node2", "node3", "node4"]
+# Edge List: [
+# {"src": "node2", "dst": "node1"},
+# {"src": "node3", "dst": "node1"},
+# {"src": "node4", "dst": "node1"},
+# ]
+@app.get("/topology")
+def get_topology():
+    node_list = ["node1", "node2", "node3", "node4"]
+
+    edge_list = [
+        {"src": "node2", "dst": "node1"},
+        {"src": "node3", "dst": "node1"},
+        {"src": "node4", "dst": "node1"},
+    ]
+
+    response_content = json.dumps({
+        "nodes": node_list,
+        "edges": edge_list,
+        "type": "graph"
+        })
+    return Response(response_content)
+
+#### EXAMPLE DATA
+# 一行一行字符串返回?
+@app.get("/flowTable/current")
+def get_current_flow_table():
+    total_entities = [
+        {
+            "dpid": "10001",
+            "entities": [
+                "flow entities 1",
+                "flow entities 2",
+                "flow entities 3",
+                "flow entities 4",
+            ],
+        },
+        {
+            "dpid": "10001",
+            "entities": [
+                "flow entities 1",
+                "flow entities 2",
+                "flow entities 3",
+                "flow entities 4",
+            ],
+        }
+    ]
+
+    response_content = json.dumps({
+        "total_entities": total_entities,
+        "type": "table"
+        })
+    return Response(response_content)
+
+#### EXAMPLE DATA
+# 一行一行字符串返回?
+@app.get("/flowTable/history")
+def get_hostory_flow_table():
+    total_entities = [
+        {
+            "dpid": "10001",
+            "entities": [
+                "flow entities 1",
+                "flow entities 2",
+                "flow entities 3",
+                "flow entities 4",
+            ],
+        },
+        {
+            "dpid": "10002",
+            "entities": [
+                "flow entities 1",
+                "flow entities 2",
+                "flow entities 3",
+                "flow entities 4",
+            ],
+        }
+    ]
+
+    response_content = json.dumps({
+        "total_entities": total_entities,
+        "type": "table"
+        })
+    return Response(response_content)
+
+#### EXAMPLE DATA
+# 
+@app.get("/statistics")
+def get_statistics():
+    total_statistics = [
+        {
+            "dpid": "10001",
+            "statistics": [
+                {
+                    "port_no": 1,
+                    "rx_packets": 10,
+                    "rx_bytes": 11,
+                    "rx_errors": 12,
+                    "tx_packets": 13,
+                    "tx_bytes": 14,
+                    "tx_errors": 15,
+                }
+            ],
+        },
+        {
+            "dpid": "10002",
+            "statistics": [
+                {
+                    "port_no": 1,
+                    "rx_packets": 10,
+                    "rx_bytes": 11,
+                    "rx_errors": 12,
+                    "tx_packets": 13,
+                    "tx_bytes": 14,
+                    "tx_errors": 15,
+                }
+            ],
+        },
+    ]
+    response_content = json.dumps({
+        "total_statistics": total_statistics,
+        "type": "table"
+        })
+    return Response(response_content)
+
+#### EXAMPLE DATA
+# 
+@app.get("/portInfo")
+def get_port_info():
+    total_port_infos = [
+        {
+            "dpid": "10001",
+            "port_info": [
+                {
+                    "port_no": 1,
+                    "port_name": "eth1",
+                    "mac": "00:00:00:00:00:01",
+                }
+            ],
+        },
+        {
+            "dpid": "10002",
+            "port_info": [
+                {
+                    "port_no": 2,
+                    "port_name": "eth2",
+                    "mac": "00:00:00:00:00:02",
+                }
+            ],
+        },
+    ]
+    response_content = json.dumps({
+        "total_port_infos": total_port_infos,
+        "type": "table"
+        })
+    return Response(response_content)
+
+#### EXAMPLE DATA
+# 
+@app.get("/macToPortTable")
+def get_mac_to_port_table():
+    total_mac_to_port = {
+        "10001": {
+            "00:00:00:00:00:01": 1,
+            "00:00:00:00:00:02": 2,
+        },
+        "10002": {
+            "00:00:00:00:00:01": 1,
+            "00:00:00:00:00:02": 2,
+        }
+    }
+
+    response_content = json.dumps({
+        "type": "table"
+        })
+    return Response(response_content)
+
+######### TMP TEST
--- rest/test/static/js/main.js
+++ rest/test/static/js/main.js
@@ -1,3 +1,84 @@
+const headerList = {"Prometheus": "http://127.0.0.1:5000"};
+const contentList = {
+  "Topology": "/topology",
+  "Flow table history": "/flowTable/history",
+  "Flow table current": "/flowTable/current",
+  "Mac to port table": "/macToPortTable",
+  "Port info": "/portInfo",
+  "Statistics": "/statistics",
+};
+
 function hello(name) {
   console.log(`Oh My ${name}`);
+}
+
+function main(wsgi) {
+  const baseUrl = wsgi.endsWith('/') ? wsgi.slice(0, -1) : wsgi;
+  const headerDiv = document.getElementsByClassName("page-header")[0];
+  const headerUl = make_ref_list(headerList);
+  headerDiv.appendChild(headerUl);
+
+  const contentListDiv = document.getElementsByClassName("content-list")[0];
+  Object.keys(contentList).forEach(key => {
+    contentList[key] = baseUrl + contentList[key];
+  })
+  const contentListUl = make_content_list(contentList);
+  contentListDiv.appendChild(contentListUl);
+}
+
+function make_ref_list(items) {
+  const ul = document.createElement('ul');
+  Object.keys(items).forEach(key => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = items[key];
+    a.textContent = key;
+    li.appendChild(a);
+    ul.appendChild(li);
+  })
+  return ul;
+}
+
+function make_content_list(items) {
+  const ul = document.createElement('ul');
+  
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const json_data = await response.json();
+      console.log("Get: ", json_data);
+      return json_data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error; // 可以选择重新抛出或处理错误
+    }
+  };
+
+  Object.keys(items).forEach(key => {
+    const li = document.createElement('li');
+    li.textContent = key;
+    
+    li.addEventListener('click', () => {
+      console.log("Clicked: ", key);
+      fetchData(items[key])
+        .then(data => {
+          // 可以在这里处理返回的数据
+          if (data.type == "graph") {
+            console.log("I should draw a graph here")
+          } else if (data.type == "table") {
+            console.log("I should make a table here")
+          }
+        })
+        .catch(error => {
+          // 错误处理逻辑
+        });
+    });
+
+    ul.appendChild(li);
+  });
+  
+  return ul;
 }
